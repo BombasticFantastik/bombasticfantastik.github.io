c09faca7383738cf5e600d317fd0ef75ae59b180
ArtemyBombastic <artemilusenko2@gmail.com>
2026-01-15 17:49:10 +0700

    добавил изменение громкости

--- sсripts/music.js
+++ sсripts/music.js
@@ -1,8 +1,9 @@
-let play= document.getElementById('player')
-const audio= new Audio
+//let play= document.getElementById('player')
 
-audio.volume=.5
+let audio= new Audio()
+
 audio.src='music/Nautilus_Pompilius_-_Knyaz_tishiny_Naugad_remastering_68595789.mp3'
+audio.volume=0.5
 
 function BackSong(){
     
@@ -19,14 +20,26 @@ function NextSong(){
     
 }
 
+// function ChangeVolume(){
+//     audio.volume=volume_input.value/100
+// }
+console.log(audio.volume)
+
 window.addEventListener('DOMContentLoaded',()=>{
-    
+
+    let volume_input=document.getElementById('volume_input');
     let back_button=document.getElementById('back_button');
     let pause_button=document.getElementById('pause_button');
     let next_button=document.getElementById('next_button');
     
+    volume_input.addEventListener('input',()=>{
+        audio.volume=volume_input.value/100
+    });
+
     back_button.onclick = ()=> BackSong();
     pause_button.onclick= ()=> PauseSong();
     next_button.onclick= ()=>NextSong();
+    //audio.volume=0.1
     
 })
+
