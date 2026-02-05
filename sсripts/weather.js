732e18a884776b23a3dc041865812d734fc6b951
ArtemyBombastic <artemilusenko2@gmail.com>
2026-02-05 10:44:39 +0700

    немного изменил структуру, добавил смайлики, добавил показатель температуры

--- sсripts/weather.js
+++ sсripts/weather.js
@@ -1,133 +1,46 @@
-const snow_canvas=document.getElementById('snow_canvas')
-const ctx=snow_canvas.getContext('2d')
+const apiKey='c27ff5019fdc192d3cfc8bc7dcd8a08f'
 
-weather='Snow'
+const weather_main_smile = document.getElementById('weather_main_smile');
+const weather_main = document.getElementById('weather_main');
+const weather_temp = document.getElementById('weather_temp');
 
-let width,height,weather_flakes=[];
-
-
-function rain_method(flake){
-    ctx.fillStyle = 'blue';
-    mthod=ctx.fillRect(flake.x, flake.y, 3, 20);
-    
-}
-function snow_method(flake){
-    ctx.fillStyle = 'grey';
-    mthod=ctx.arc(flake.x, flake.y, flake.size, 0, Math.PI * 2);
-    
-}
-//let mthod=draw_snow_flake
-
-function init(){
-    width = window.innerWidth;
-    height = window.innerHeight;
-    snow_canvas.width = width;
-    snow_canvas.height = height;
-    weather_flakes=[];
-    for (let i = 0;i<150;i++){
-        weather_flakes.push(new WeatherFlake(snow_method,rain_method))
-    }
-    //console.log(1)
-}
-class WeatherFlake{
-    constructor(snow_method,rain_method){
-        this.reset()
-        this.snow_method=snow_method
-        this.rain_method=rain_method
-        this.mthod=snow_method
-    }
-    reset(){
-        this.x=Math.random()*width
-        this.y=Math.random()*height
-        this.size=Math.random()*3+1
-        this.speed=Math.random()*1+0.5
-        this.velX=Math.random()*0.5-0.25
-    }
-    update(){
-        this.x+=this.velX
-        this.y+=this.speed
-
-        if ((this.y>height)&&(weather)!='Clear' ) {
-            this.y=-100;
-            this.x=Math.random()*width
-            this.on_screen=true
+async function getWeather(){
+    const city='Kuragino'
+    const url= `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=ru`;
+    try {
+        const response= await fetch(url)
+        if (!response.ok){
+            console.log('ГОРОДА НЕТ')
         }
-    }
-    draw(){        
-        ctx.beginPath();
-        if (this.on_screen) {
-            
-            
-            if (weather=='Snow'){
-                this.speed=Math.random()*1+0.5
-                this.mthod=this.snow_method
-            }
-            else if(weather=='Rain'){
-                this.speed=Math.random()+9
-                this.mthod=this.rain_method
-                
-            }
+        const data = await response.json()
+        console.log(data)
+
+        //погода и смайл
+        if (data.weather[0].main=="Clouds"){
+            main="Clouds"
+            smile="☁️"
         }
-        //this.mthod=draw_rain_flake
-        //ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-        this.mthod(this)
-        this.on_screen=false
-        
-        
-        
-        ctx.fill();
-
-        
-    }
-}
-
-class Rainflake{
-    constructor(){
-        this.reset()
-    }
-    reset(){
-        this.x=Math.random()*width
-        this.y=Math.random()*height
-        this.size=Math.random()*3+1
-        this.speed=Math.random()*1+2
-        this.velX=Math.random()*0.5-0.25
-    }
-    update(){
-        this.x+=this.velX
-        this.y+=this.speed
-
-        if ((this.y>height)&&(weather)=='Rain' ) {
-            this.y=-10;
-            this.x=Math.random()*width
+        else if (data.weather[0].main=="Sunny"){
+            main="Sunny"
+            smile="☀️"
         }
+        
+        //температура
+        if (data.main.temp<-15){
+            temp=data.main.temp+"🥶"
+        }
+        else if (data.main.temp<0){
+            temp=data.main.temp+"❄️"
+        }
+        
+        weather_main_smile.textContent=smile
+        weather_main.textContent=main
+        weather_temp.textContent=temp
+        
     }
-    draw(){
-        ctx.beginPath();
-        ctx.fillRect(this.x, this.y, 2, 20);
-        ctx.fillStyle = 'grey';
-        ctx.fill();
-    }
+    catch (error){
+        console.log(error)
+    }+"❄️"+"☁️"
 }
-function animate_weather(){
-    ctx.clearRect(0, 0, width, height);
-    weather_flakes.forEach(flake => {
-    flake.update();
-    flake.draw();
-    
-  });
-  requestAnimationFrame(animate_weather);
-}
-window.addEventListener('resize',init);
 
-init()
-animate_weather()
-
-
-
-
-//animate_snow()
-
-document.querySelectorAll('input[type="radio"][name="weather"]').forEach(radio => {
-    radio.addEventListener('change', () => weather=radio.value);
-    //console.log(weather)
-});
+getWeather()
