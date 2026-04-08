cab53f382b242ef2074b9271088a008d8ca4c087
ArtemyBombastic <artemilusenko2@gmail.com>
2026-04-08 18:09:01 +0700

    добавил двд скрипт и он должен работать (наверное)

---
+++ sсripts/dvd_script.js
@@ -0,0 +1,8 @@
+window.addEventListener('DOMContentLoaded',()=>{
+
+    let dvd_button=document.getElementById('dvd_button');
+    let dvd_sign=document.getElementById('dvd_sign');
+
+    dvd_button.onclick = ()=> dvd_sign.hidden=false
+    
+})
