7d0dba7a9d7473c013401569d5bf96f2585411dd
ArtemyBombastic <artemilusenko2@gmail.com>
2026-04-11 10:08:21 +0700

    добавил крутой взрыв

--- sсripts/dvd_script.js
+++ sсripts/dvd_script.js
@@ -1,20 +1,17 @@
 document.addEventListener('click',(event)=>{
 
     if (event.target.id=="dvd_button"){
-        //let dvd_button=window.getElementById("dvd_button");
         let dvd_sign=document.getElementById("dvd_sign");
         let dvd_sign_img=document.getElementById("dvd_sign_img");
         
         dvd_sign.hidden=false
-        
-        dvd_sign_img.onclick = ()=> dvd_sign.hidden=true//.style.display='block'
+        dvd_sign_img.onclick = ()=> {
+            dvd_sign_img.src = "gifs/boom0.gif?t=" + new Date().getTime();
+            setTimeout(() => {
+                dvd_sign.hidden=true;
+            }, 2150);
+        }
+        dvd_sign_img.src="images/dvd_logo.png"
+
     }
-    
-    // let dvd_button=window.getElementById("dvd_button");
-    // let dvd_sign=window.getElementById("dvd_sign");
-
-    // console.log(dvd_button)
-
-    //dvd_button.onclick = ()=> dvd_sign.hidden=false//.style.display='block'
-    
 })
