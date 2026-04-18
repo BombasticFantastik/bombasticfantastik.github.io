22ef9e7d6965914585050d3db584af54f1c3aba1
ArtemyBombastic <artemilusenko2@gmail.com>
2026-04-18 12:00:35 +0700

    добавил секретный трек

---
+++ sсripts/рэбscript.js
@@ -0,0 +1,21 @@
+
+//первый 88x31
+let ew_88x31=document.getElementById("ew_88x31");
+console.log(ew_88x31)
+
+ew_88x31.onclick = ()=> {
+    song_name.textContent='Secret track 0'
+    audio.src='other_files/ElectronicWarfare.mp3'
+    PauseSong()//
+    
+}
+
+//второй 88x31
+let ew_88x312=document.getElementById("ew_88x312");
+console.log(ew_88x312)
+
+ew_88x312.onclick = ()=> {
+    song_name.textContent='Secret track 0'
+    audio.src='other_files/ElectronicWarfare.mp3'
+    PauseSong()
+}
