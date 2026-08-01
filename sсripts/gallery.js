1fe4342a1eaae467be82e29761eac1918ee0dfc4
BombasticFantastic <artemilusenko2@gmail.com>
2026-08-01 18:40:38 +0700

    добавил случайное изображение

--- sсripts/gallery.js
+++ sсripts/gallery.js
@@ -1,19 +1,28 @@
-function LoadPieceOfPage(url){
+async function LoadPieceOfPage(url){
     const contentArea = document.getElementById('gallery_content_area') 
-    fetch(url)
-        .then(response => response.text())
-        .then(html=>{
-            contentArea.innerHTML=html
-            reinit()
-        });
+    const response = await fetch(url)
+    const html=await response.text()
+
+    contentArea.innerHTML=html
+    reinit()
 }
+function GetRandomNumHorizontal(){
+    return Math.random()*1000
+}
+
 const main_content=document.getElementById('content-area') 
 
-main_content.addEventListener('click',(event)=>{
+
+main_content.addEventListener('click',async (event)=>{
     const gallery_button=event.target.closest('.gallery_button')
     if (gallery_button){
-        //console.log('sad')
-        LoadPieceOfPage('gallery.html')
+        
+        await LoadPieceOfPage('gallery.html')
+
+        //отрисовка изображений
+        const zamn_bikini=document.getElementById('zamn_bikini_drawing')
+        zamn_bikini.style.marginLeft=GetRandomNumHorizontal()+'px'
+
     }
 
 })
